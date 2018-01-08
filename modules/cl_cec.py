6b5784069d8712e9816521107b54d88997010758
Michal <mike.muransky@gmail.com>
2018-01-08 22:58:22 +0100

    Add CEC modul

---
+++ modules/cl_cec.py
@@ -0,0 +1,22 @@
+# System modules
+import sys
+import cec
+import ConfigParser
+
+# Read credentials
+config = ConfigParser.ConfigParser()
+config.read("/etc/max.conf")
+ADAPTER = config.get("cl_cec", "adapter")
+
+def cecList()
+   print('Oops!')
+
+def cecState()
+   print('Oops!')
+
+def cecOn()
+   print('Oops!')
+
+def cecOff()
+   print('Oops!')
+
